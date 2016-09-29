1d24d20955aceffc8890e4f9ab240cae3ef4784a
HyeonJu Jeong <0106grace@naver.com>
2016-09-29 20:16:42 +0900

    #5 Complete Event Programming

--- app/src/main/java/kr/hs/e_mirim/grace0106/touchgraphic/MainActivity.java
+++ app/src/main/java/kr/hs/e_mirim/grace0106/touchgraphic/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     static final int LINE=1, RECT=2, CIRCLE=3;
+    int chooseShape = CIRCLE;
     DrawShape ds;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +25,16 @@ public class MainActivity extends AppCompatActivity {
     public void drawShape(View v) {
         switch(v.getId()) {
             case R.id.but_line:
+                chooseShape = LINE;
                 break;
             case R.id.but_rect:
+                chooseShape = RECT;
                 break;
             case R.id.but_circle:
+                chooseShape = CIRCLE;
                 break;
         }
+        ds.invalidate();
     }
 
     class DrawShape extends View {
@@ -45,10 +50,20 @@ public class MainActivity extends AppCompatActivity {
             paint.setStrokeWidth(7);
             paint.setColor(Color.BLUE);
             paint.setStyle(Paint.Style.STROKE);
-            canvas.drawCircle(cx, cy, 200, paint);
-            paint.setColor(Color.MAGENTA);
-            paint.setStyle(Paint.Style.FILL);
-            canvas.drawRect(100, 100, 500, 250, paint);
+            switch (chooseShape) {
+                case LINE:
+                    canvas.drawLine(50, 100, 650, 100, paint);
+                    break;
+                case RECT:
+                    paint.setColor(Color.MAGENTA);
+                    paint.setStyle(Paint.Style.FILL);
+                    canvas.drawRect(100, 100, 500, 250, paint);
+                    break;
+                case CIRCLE:
+                    canvas.drawCircle(cx, cy, 200, paint);
+                    break;
+            }
+
         }
     }
 }
